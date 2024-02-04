624e99b977093dd0d88b7d6a406c02b6bc90a3a6
puzzlejoy <129676917+puzzlejoy@users.noreply.github.com>
2024-02-04 22:04:23 +0500

    Update DM_installation.sh

--- DM_installation.sh
+++ DM_installation.sh
@@ -5,6 +5,42 @@ echo "Sourcelist update"
 astraVersion=$(cat /etc/astra_version)
 echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-base/ 1.7_x86-64 main contrib non-free" > /etc/apt/sources.list
 echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-main/ 1.7_x86-64 main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://dl.astralinux.ru/astra/stable/1.7_x86-64/1.7.3/repository-extended/ 1.7_x86-64 contrib main non-free " >> /etc/apt/sources.list
 apt update
 cat /etc/apt/sources.list
 echo "Done!"
+
+# Addition to AD 
+apt-get install fly-admin-ad-client -y
+apt update
+fly-admin-ad-client
+
+# /etc/hosts change
+echo "Working with the /etc/hosts file..."
+echo "Enter domain: "; read ADdomain
+urIP=$(ifconfig eth0 | awk '/inet / {split($2, a, ":"); print a[1]}')
+hostname=$(hostname)
+newEntry="$urIP $hostname.$ADdomain $hostname"
+sed -i "2s/.*/$newEntry/" /etc/hosts
+echo "Done!"
+
+# SSH
+systemctl start ssh && systemctl enable ssh
+echo "The SSH service will be start and added to autostart"
+
+# .NET 6 Installation 
+"Installing .NET, socat and contract..."
+apt install ca-certificates apt-transport-https -y
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null 
+wget https://packages.microsoft.com/config/debian/10/prod.list -O /etc/apt/sources.list.d/microsoft-prod.list 
+apt update 
+apt install dotnet-sdk-6.0 aspnetcore-runtime-6.0 -y
+apt install conntrack socat -y
+
+# PostgreSQL installation
+echo "Installation PostgreSQL"
+apt-get install postgres
+sudo su - postgres
+psql -c "alter user postgres with password 'postgres'" 
+exit
+
