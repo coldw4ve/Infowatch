ce6eb0bc181bab51fee2b49e0c054aa87c604915
puzzlejoy <129676917+puzzlejoy@users.noreply.github.com>
2024-02-04 22:51:03 +0500

    Update DM_installation.sh

--- DM_installation.sh
+++ DM_installation.sh
@@ -2,13 +2,16 @@
 
 # Sources list
 echo "Sourcelist update"
-astraVersion=$(cat /etc/astra_version)
-echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-base/ 1.7_x86-64 main contrib non-free" > /etc/apt/sources.list
-echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-main/ 1.7_x86-64 main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://dl.astralinux.ru/astra/stable/1.7_x86-64/1.7.3/repository-extended/ 1.7_x86-64 contrib main non-free " >> /etc/apt/sources.list
-apt update
-cat /etc/apt/sources.list
-echo "Done!"
+if
+  astraVersion=$(cat /etc/astra_version)
+  echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-base/ 1.7_x86-64 main contrib non-free" > /etc/apt/sources.list
+  echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-main/ 1.7_x86-64 main contrib non-free" >> /etc/apt/sources.list
+  echo "deb http://dl.astralinux.ru/astra/stable/1.7_x86-64/1.7.3/repository-extended/ 1.7_x86-64 contrib main non-free " >> /etc/apt/sources.list
+  apt update
+  cat /etc/apt/sources.list
+then
+  echo "Done!"
+fi
 
 # Addition to AD 
 apt-get install fly-admin-ad-client -y
@@ -16,13 +19,16 @@ apt update
 fly-admin-ad-client
 
 # /etc/hosts change
-echo "Working with the /etc/hosts file..."
-echo "Enter domain: "; read ADdomain
-urIP=$(ifconfig eth0 | awk '/inet / {split($2, a, ":"); print a[1]}')
-hostname=$(hostname)
-newEntry="$urIP $hostname.$ADdomain $hostname"
-sed -i "2s/.*/$newEntry/" /etc/hosts
-echo "Done!"
+if
+  echo "Working with the /etc/hosts file..."
+  echo "Enter domain: "; read ADdomain
+  urIP=$(ifconfig eth0 | awk '/inet / {split($2, a, ":"); print a[1]}')
+  hostname=$(hostname)
+  newEntry="$urIP $hostname.$ADdomain $hostname"
+  sed -i "2s/.*/$newEntry/" /etc/hosts
+then  
+  echo "Done!"
+fi
 
 # SSH
 systemctl start ssh && systemctl enable ssh
@@ -30,17 +36,47 @@ echo "The SSH service will be start and added to autostart"
 
 # .NET 6 Installation 
 "Installing .NET, socat and contract..."
-apt install ca-certificates apt-transport-https -y
-wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null 
-wget https://packages.microsoft.com/config/debian/10/prod.list -O /etc/apt/sources.list.d/microsoft-prod.list 
-apt update 
-apt install dotnet-sdk-6.0 aspnetcore-runtime-6.0 -y
-apt install conntrack socat -y
+if
+  apt install ca-certificates apt-transport-https -y
+  wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null 
+  wget https://packages.microsoft.com/config/debian/10/prod.list -O /etc/apt/sources.list.d/microsoft-prod.list 
+  apt update 
+  apt install dotnet-sdk-6.0 aspnetcore-runtime-6.0 -y
+  apt install conntrack socat -y
+then
+  echo "Success!"
+fi
+
 
 # PostgreSQL installation
 echo "Installation PostgreSQL"
-apt-get install postgres
-sudo su - postgres
-psql -c "alter user postgres with password 'postgres'" 
-exit
+  apt-get install postgres
+if
+  read "Enter PostgreSQL Password: " $postgrePass
+  sudo -u postgres psql -c "alter user postgres with password '$postgrePass'"
+  exit
+then
+  echo "PostgreSQL is Done, password is $postgrePass!"
+fi
+
+# Sorting IWDM files
+echo "Moving IWDM-installer files..."
+if 
+  echo "Enter .zip archive file name"; read zipFolder
+  mkdir IWDM/
+  unzip -j $zipFolder;
+  mv i* IWDM/
+  cd IWDM/
+then
+  echo "Done!"
+fi
+
+# Platform Installation
+echo "Platform IWDM installation..."
+tar xvf iw_devicemonior_setup*
+./setup.py install
+kubectl get pods -n infowatch 
+kubectl get configmap nginx-config -o yaml -n infowatch > n.yaml
+
+
 
