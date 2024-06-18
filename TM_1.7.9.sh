4d3db5ed389ceaf21ccc547c15fc7f5529b738a2
coldwave <129676917+coldw4ve@users.noreply.github.com>
2024-06-18 16:47:50 +0500

    Update TM_1.7.9.sh

--- TM_1.7.9.sh
+++ TM_1.7.9.sh
@@ -1,36 +1,45 @@
 #! /bin/bash
 
 # Sources list
-
+echo "deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-main/ 1.7_x86-64 main contrib non-free" >> /etc/hosts
+echo "deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-update/ 1.7_x86-64 main contrib non-free" >> /etc/hosts
+echo "deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/ 1.7_x86-64 main contrib non-free" >> /etc/hosts
+echo "deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-extended/ 1.7_x86-64 main contrib non-free" >> /etc/hosts
 apt update
 
+## If youre use VMWare install VMWare Tools automatically
+mkdir /mnt/vmware
+mount /dev/cdrom /mnt/vmware
+cp /mnt/vmware/VMwareTools-*.tar.gz /tmp/
+cd /tmp
+tar -xzf VMwareTools-*.tar.gz
+./vmware-tools-distrib/vmware-install.pl 
 
 # TM packages
-"Installing additional packages for IWTM..."
 apt install lsb-release lshw ntp ntpdate gsfonts libjemalloc2 libnewt0.52 libwmf-bin libwmf0.2-7 libxml2-utils liblzf1 python-newt redis-server redis-tools
 
 # /etc/hosts change
-echo "Working with the /etc/hosts file..."
 echo "Enter domain: "; read ADdomain
 urIP=$(ifconfig eth0 | awk '/inet / {split($2, a, ":"); print a[1]}')
 hostname=$(hostname)
 newEntry="$urIP $hostname.$ADdomain $hostname"
 sed -i "2s/.*/$newEntry/" /etc/hosts
-echo "Done!"
 
 # SSH
 systemctl start ssh && systemctl enable ssh
-echo "The SSH service will be start and added to autostart"
 
 # Mandate regime
-echo "Max access mode set..."
 pdpl-user -i 63 root
 
-mkdir /distr
-mv iwtm* /distr
-cd /distr
+# TM files movement
+7z x *.zip
+mkdir /distr/
+mv iwtm* /distr/
+mv opt_packages/ /distr/
+cd /distr/
 chmod +x iwtm-installer*
 
+# /etc/systemd/system/rsyslog.service file editing
 cat << EOF > /etc/systemd/system/rsyslog.service
 [Service]
 ExecStart=true
@@ -42,12 +51,11 @@ EOF
 
 systemctl daemon-reload
 
+# SSH
 systemctl enable sshd && systemctl start sshd
 
 # Cache cleaning
-apt-cache show astra-version
 apt clean
-echo "Cache cleared..."
 
 ./iwtm-installer*
 
